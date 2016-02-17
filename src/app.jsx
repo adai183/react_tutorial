4216a56526f91ad051c5b89dad18e871843b7a25
adai183 <andreas.daiminger@gmail.com>
2016-02-17 23:29:21 +0100

    Implement a controlled form

--- src/app.jsx
+++ src/app.jsx
@@ -2,19 +2,24 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Reactfire = require('reactfire');
 var Firebase = require('firebase');
-var rooturl = 'https://glowing-torch-1814.firebaseio.com/';
+var rooturl = 'https://glowing-torch-1814.firebaseio.com/'
+var Header = require('./header');
 
-var Hello = React.createClass({
+var App = React.createClass({
   mixins: [Reactfire],
   componentWillMount: function(){
     this.bindAsObject(new Firebase(rooturl + 'items/'), 'items');
   },
   render: function() {
-    console.log(this.state);
-    return <h1 className="red">
-      Hello!
-    </h1>
+    return <div className="row panel panel-default">
+      <div className="col-md-8 col-md-offset-2">
+        <h2 className="text-center">
+          To-Do List
+        </h2>
+        <Header />
+      </div>
+    </div>
   }
 });
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 ReactDOM.render(element, document.querySelector('.container'));
